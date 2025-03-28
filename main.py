51f3946339208c0e699164e9c053b02ba3dc028e
MikhailBasiy <mikhail.basiy@gmail.com>
2025-03-28 15:02:46 +0300

    refactor main module

--- main.py
+++ main.py
@@ -5,18 +5,12 @@ from get_db_prices import get_db_prices
 
 def prepare_site_data(site_prices: pd.DataFrame) -> pd.DataFrame:
     site_prices["Название_карточки"] = site_prices["Название_карточки"].str.replace("Шкаф-купе ", "", )
-    site_prices[""]
+    site_prices["Цвет_профиля"] = site_prices["Цвет_профиля"].str.replace(" профиль", "")
+    return site_prices
 
 
-def main():
-    site_prices: pd.DataFrame = get_site_prices()
-    # site_prices = pd.read_excel("output.xlsx")
-    db_prices: pd.DataFrame = get_db_prices()
-
-    # site_prices.to_excel("site_prices.xlsx", engine="xlsxwriter", index=False)
-    # db_prices.to_excel("db_prices.xlsx", engine="xlsxwriter", index=False)
-
-    result = site_prices.merge(
+def join_tables(site_prices:pd.DataFrame, db_prices: pd.DataFrame) -> pd.DataFrame:
+    joined_tables = site_prices.merge(
         db_prices, 
         how="left", 
         left_on=[
@@ -39,9 +33,19 @@ def main():
         ],
         suffixes=('_сайт', '_БД')
     )
+    return joined_tables
+
+
+def main():
+    # site_prices: pd.DataFrame = prepare_site_data(get_site_prices())
+    site_prices = prepare_site_data(pd.read_excel("output.xlsx"))
+    db_prices: pd.DataFrame = get_db_prices()
+    result = join_tables(site_prices, db_prices)
+
+    # site_prices.to_excel("site_prices.xlsx", engine="xlsxwriter", index=False)
+    # db_prices.to_excel("db_prices.xlsx", engine="xlsxwriter", index=False)
 
     result.to_excel("result.xlsx", index=False, engine="xlsxwriter")
-
 
 
 if __name__ == "__main__":
