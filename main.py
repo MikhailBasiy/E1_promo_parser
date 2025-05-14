bc0c4931e37959633503c992f549850a898056fb
MikhailBasiy <mikhail.basiy@gmail.com>
2025-05-14 12:18:33 +0300

    refactor: minor changes

--- main.py
+++ main.py
@@ -12,17 +12,6 @@ def compare_prices(data: pd.DataFrame) -> pd.DataFrame:
     return data
 
 
-# def prepare_site_data(site_prices: pd.DataFrame) -> pd.DataFrame:
-#     site_prices["Название_карточки"] = site_prices["Название_карточки"].str.replace(
-#         "Шкаф-купе ",
-#         "",
-#     )
-#     site_prices["Цвет_профиля"] = site_prices["Цвет_профиля"].str.replace(
-#         " профиль", ""
-#     )
-#     return site_prices
-
-
 def join_tables(site_prices: pd.DataFrame, db_prices: pd.DataFrame) -> pd.DataFrame:
     joined_tables = site_prices.merge(
         db_prices,
@@ -51,14 +40,7 @@ def join_tables(site_prices: pd.DataFrame, db_prices: pd.DataFrame) -> pd.DataFr
 
 
 def main():
-    # site_prices: pd.DataFrame = prepare_site_data(get_site_prices())
-    # site_prices = prepare_site_data(pd.read_excel("site_prices.xlsx"))       #TODO: on prod replace with the row above
     get_site_prices()
-    # fdate = date.today().strftime("%b-%d-%Y")
-    # site_prices.to_excel(
-    #     f"collected_prices/prices_{fdate}.xlsx", engine="xlsxwriter", index=False
-    # )
-
     # db_prices: pd.DataFrame = get_db_prices()
     # db_prices.to_excel("db_prices.xlsx", engine="xlsxwriter", index=False)
 
