77ec662265b12d0bd0481712b9c63d3fff63b92a
MikhailBasiy <mikhail.basiy@gmail.com>
2025-05-14 11:34:52 +0300

    style: format files

--- get_site_prices.py
+++ get_site_prices.py
@@ -10,8 +10,9 @@ from selenium.common.exceptions import (NoSuchElementException,
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from db_engine import get_engine
 from sqlalchemy import NVARCHAR, Integer
+
+from db_engine import get_engine
 
 scraping_settings = {
     "XPATH": '//div[@class="prices-wrapper"]/div/span/span[@class="price_value"]',
@@ -32,11 +33,17 @@ def write_to_db(data: pd.DataFrame) -> None:
         "Цвет_профиля": NVARCHAR(255),
         "Компоновка": NVARCHAR(255),
         "URL": NVARCHAR(500),
-        "Цена": Integer()
+        "Цена": Integer(),
     }
     engine = get_engine("E-COM")
     with engine.connect() as con:
-        data.to_sql("Результат_Стоимость_шкафов_с_промо_Сайт", con=con, dtype=column_types, index=False, if_exists="replace")
+        data.to_sql(
+            "Результат_Стоимость_шкафов_с_промо_Сайт",
+            con=con,
+            dtype=column_types,
+            index=False,
+            if_exists="replace",
+        )
 
 
 def normalize_price(price: str) -> int | str:
